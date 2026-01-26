168a0a43802166caef4bf2cefc6e9448c4d513ac
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-01-26 16:33:48 +0200

    minor improvement

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return ( 
     <main className="h-screen w-screen overflow-hidden">
       <iframe
+
         src="/city-3d/dist/index.html"
         className="w-full h-full border-0"
         allowFullScreen
