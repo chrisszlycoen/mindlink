158587f50d8b36af03e3e99e874b7fc1acd13913
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-02-05 20:09:28 +0200

    minor update

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 
-import "./globals.css";
 import type { Metadata } from "next"; 
+import "./globals.css";
 
 const geistSans = Geist({ 
   variable: "--font-geist-sans",
