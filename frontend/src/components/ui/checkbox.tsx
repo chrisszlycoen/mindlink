e3757ccd655816566e56b84216f513b12e1aec01
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-02-11 16:50:36 +0200

    improve readability

--- frontend/src/components/ui/checkbox.tsx
+++ frontend/src/components/ui/checkbox.tsx
@@ -6,7 +6,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-function Checkbox({
+function Checkbox({ 
   className,
   ...props
 }: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
