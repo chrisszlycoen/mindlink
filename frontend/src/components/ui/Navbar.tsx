64a739c2dcf56297d792eb78331a5c19d6f29f86
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-02-22 09:31:03 +0200

    minor improvement

--- frontend/src/components/ui/Navbar.tsx
+++ frontend/src/components/ui/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Link from 'next/link';
 import React, { useEffect, useState } from "react";
+import Link from 'next/link';
 import Image from "next/image";
 import { Mail } from "lucide-react";
 import { motion } from "framer-motion";
